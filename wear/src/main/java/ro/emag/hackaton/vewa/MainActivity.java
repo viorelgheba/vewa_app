79c25d6250291bf8581e61ba2e52a778a57b5525
Laurentiu Matei <lmatei@gmail.com>
2015-06-13 13:03:33 +0300

    Added gear speech recognition

--- wear/src/main/java/ro/emag/hackaton/vewa/MainActivity.java
+++ wear/src/main/java/ro/emag/hackaton/vewa/MainActivity.java
@@ -1,13 +1,19 @@
 package ro.emag.hackaton.vewa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+
+    private static final int SPEECH_RECOGNIZER_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,31 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                startSpeechRecognition();
             }
         });
     }
+
+    private void startSpeechRecognition() {
+        // Create an intent to start the Speech Recognizer
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity
+        startActivityForResult(intent, SPEECH_RECOGNIZER_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_RECOGNIZER_REQUEST_CODE) {
+            // When the speech recognizer finishes its work, Android invokes this callback with requestCode equal to SPEECH_RECOGNIZER_REQUEST_CODE
+            if (resultCode == RESULT_OK) {
+                List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String recognizedText = results.get(0);
+                // Display the recognized text
+                mTextView.setText(recognizedText);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
